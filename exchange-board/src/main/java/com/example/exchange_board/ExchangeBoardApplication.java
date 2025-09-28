6aeccc7a18067d4949b5c685e74035bd3e581cdf
zoshigaya <omuraisu893@gmail.com>
2025-09-28 21:52:04 +0900

    initial commit

---
+++ exchange-board/src/main/java/com/example/exchange_board/ExchangeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.exchange_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeBoardApplication.class, args);
+	}
+
+}
---
+++ exchange-board/src/test/java/com/example/exchange_board/ExchangeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchange_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
